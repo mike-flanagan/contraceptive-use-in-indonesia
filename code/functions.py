1a94166fa17e9785521ba42358ba6e7e86cb4d4d
Wei Alexander Xin <eggrollofchaos@gmail.com>
2021-03-29 17:40:38 -0400

    contraceptive

--- code/functions.py
+++ code/functions.py
@@ -1,8 +1,10 @@
 
-def csv_write(ticker, df):
+def csv_write(ticker, df, start, end, intra = ''):
     '''Given a target filepath and a list of fields,
     create a new CSV file and set Headers
     '''
-    df.columns = ['open', 'high', 'low', 'close', 'volume']
-    csv_filepath = f'data/{ticker}_intra_Mar_15_26.csv'
+    df.columns = ['open', 'high', 'low', 'close', 'change', 'volume']
+    if intra = 'y':
+        intra = 'intra'
+    csv_filepath = f'data/{ticker}_{intra}_{start}_{end}.csv'
     df.to_csv(csv_filepath, mode='w')
